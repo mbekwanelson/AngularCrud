5ff77c97219cc01434cfee557297acfcfb70a662
Ntsikelelo.Mbekwa <mbekwantsikelelonelson@gmail.com>
2023-03-11 22:27:48 +0200

    Update Dialog name

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { IDialogData } from '../model/idialog-data';
+import { IDialogData } from '../model/IDialogData';
 
 @Component({
   selector: 'app-dialog',
